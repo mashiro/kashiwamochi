96ced26019e108454bc27132b1cd3ee7d6036954
mashiro <mail@mashiro.org>
2012-01-10 15:28:29 +0900

    Remove deprecated InstanceMethods scope

--- lib/kashiwamochi/action_controller.rb
+++ lib/kashiwamochi/action_controller.rb
@@ -2,14 +2,10 @@ module Kashiwamochi
   module ActionController
     extend ActiveSupport::Concern
 
-    module InstanceMethods
-
-      def build_search_query!
-        q = params.delete(Kashiwamochi.config.search_key)
-        instance_variable_set("@#{Kashiwamochi.config.search_key}", Kashiwamochi.build(q))
-      end
-      alias_method :build_query!, :build_search_query! unless defined?(build_query!)
-
+    def build_search_query!
+      q = params.delete(Kashiwamochi.config.search_key)
+      instance_variable_set("@#{Kashiwamochi.config.search_key}", Kashiwamochi.build(q))
     end
+    alias_method :build_query!, :build_search_query! unless defined?(build_query!)
   end
 end
--- lib/kashiwamochi/action_view.rb
+++ lib/kashiwamochi/action_view.rb
@@ -2,50 +2,46 @@ module Kashiwamochi
   module ActionView
     extend ActiveSupport::Concern
 
-    module InstanceMethods
+    def search_form_for(query, *args, &block)
+      options = args.extract_options!
 
-      def search_form_for(query, *args, &block)
-        options = args.extract_options!
-
-        options[:url] ||= url_for
-        options[:as] ||= Kashiwamochi.config.search_key
-        options[:method] ||= :get
-        options[:html] ||= {}
-        options[:html].tap do |html|
-          html[:id] ||= "#{options[:as]}_#{Kashiwamochi.config.form_class}"
-          html[:class] = [
-            html[:class] || "#{options[:as]}_#{Kashiwamochi.config.form_class}",
-            "#{Kashiwamochi.config.form_class}"
-          ].compact.join(' ')
-          html[:method] ||= :get
-        end
-
-        form_method = options.delete(:form_method) || Kashiwamochi.config.form_method
-        send(form_method, query, *(args << options), &block)
-      end
-
-      def search_sort_link_to(query, attribute, *args)
-        options = args.extract_options!
-        html_options = options.delete(:html_options) || {}
-        default_order = options.delete(:default_order)
-
-        attr_name = attribute.to_s
-        sort = query.sort_params[attr_name] || Kashiwamochi::Sort.new(attr_name, default_order)
-
-        html_options[:class] = [
-          html_options[:class] || "#{attr_name}_#{Kashiwamochi.config.sort_link_class}",
-          Kashiwamochi.config.sort_link_class,
-          sort.dir.downcase
+      options[:url] ||= url_for
+      options[:as] ||= Kashiwamochi.config.search_key
+      options[:method] ||= :get
+      options[:html] ||= {}
+      options[:html].tap do |html|
+        html[:id] ||= "#{options[:as]}_#{Kashiwamochi.config.form_class}"
+        html[:class] = [
+          html[:class] || "#{options[:as]}_#{Kashiwamochi.config.form_class}",
+          "#{Kashiwamochi.config.form_class}"
         ].compact.join(' ')
-
-        query.sort_params[attr_name] = sort.toggle!
-        options[Kashiwamochi.config.search_key] = query.to_option
-
-        name = args.shift || attr_name
-        link_to(name, options, html_options)
+        html[:method] ||= :get
       end
-      alias_method :sort_link_to, :search_sort_link_to unless defined?(sort_link_to)
 
+      form_method = options.delete(:form_method) || Kashiwamochi.config.form_method
+      send(form_method, query, *(args << options), &block)
     end
+
+    def search_sort_link_to(query, attribute, *args)
+      options = args.extract_options!
+      html_options = options.delete(:html_options) || {}
+      default_order = options.delete(:default_order)
+
+      attr_name = attribute.to_s
+      sort = query.sort_params[attr_name] || Kashiwamochi::Sort.new(attr_name, default_order)
+
+      html_options[:class] = [
+        html_options[:class] || "#{attr_name}_#{Kashiwamochi.config.sort_link_class}",
+        Kashiwamochi.config.sort_link_class,
+        sort.dir.downcase
+      ].compact.join(' ')
+
+      query.sort_params[attr_name] = sort.toggle!
+      options[Kashiwamochi.config.search_key] = query.to_option
+
+      name = args.shift || attr_name
+      link_to(name, options, html_options)
+    end
+    alias_method :sort_link_to, :search_sort_link_to unless defined?(sort_link_to)
   end
 end
