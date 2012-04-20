78f75ea859c632644095b64fd01e28956a9bc5c0
mashiro <mail@mashiro.org>
2012-04-20 15:54:10 +0900

    Fix load order

--- lib/kashiwamochi.rb
+++ lib/kashiwamochi.rb
@@ -1,9 +1,10 @@
 require "kashiwamochi/version"
-require 'kashiwamochi/railtie' if defined?(Rails)
 
 module Kashiwamochi
   autoload :Configuration, 'kashiwamochi/configuration'
   autoload :Search,        'kashiwamochi/search'
   autoload :Sort,          'kashiwamochi/sort'
   autoload :Query,         'kashiwamochi/query'
+
+  require 'kashiwamochi/railtie' if defined?(Rails)
 end
