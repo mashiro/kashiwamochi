063ab0a754430bed60a660b55d11786da3516a59
mashiro <mail@mashiro.org>
2013-06-27 15:19:26 +0900

    support rails 4.x

--- spec/fake_app.rb
+++ spec/fake_app.rb
@@ -7,6 +7,8 @@ ActiveRecord::Base.establish_connection('test')
 
 app = Class.new(Rails::Application)
 app.config.active_support.deprecation = :log
+app.config.secret_key_base = '15314f4ae1fb4cb0a2871cb8f9c71fb3ef18edeb0488913656c87197e27c9ab014009ddef85e5420e8973d2972f9ec5a1449137b63d0c35511182faee29d12ce'
+app.config.eager_load = false
 app.initialize!
 
 app.routes.draw do
